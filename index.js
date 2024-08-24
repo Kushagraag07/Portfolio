a6f1b7698b4c1732218b4c285e31952b63bf618d
Masktech07 <ka9424@srmist.edu.in>
2024-08-24 12:00:00 +0530

    Improved event handling and applied event delegation in JavaScript

--- index.js
+++ index.js
@@ -14,23 +14,25 @@ function opentab(tabname, event) {
 
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_items = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+// Toggle hamburger menu and mobile menu
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
 
+// Event delegation for menu item clicks
+mobile_menu.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    }
+});
+
+// Change header background on scroll
 document.addEventListener('scroll', () => {
     const scroll_position = window.scrollY;
     header.style.backgroundColor = scroll_position > 250 ? '#29323c' : 'transparent';
 });
 
-// new
-menu_items.forEach((item) => {
-    item.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-        mobile_menu.classList.toggle('active');
-    });
-});
